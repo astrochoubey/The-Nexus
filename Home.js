f1f536ebe7593f1b17c0923619dc080a1a4055c8
Prachi choubey <prachichoubey@RealSlimShady.local>
2025-12-21 20:27:09 +0530

    nasa api

---
+++ Home.js
@@ -0,0 +1,23 @@
+const API_KEY = "ecRi7V9yMIkD0aznfrZ4BhXxUM6auiftktId5Yge"; // or DEMO_KEY
+const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("apod-title").textContent = data.title;
+    document.getElementById("apod-desc").textContent = data.explanation;
+
+    const img = document.getElementById("apod-img");
+    const video = document.getElementById("apod-video");
+
+    if (data.media_type === "image") {
+      img.src = data.url;
+      img.style.display = "block";
+      video.style.display = "none";
+    } else if (data.media_type === "video") {
+      video.src = data.url;
+      video.style.display = "block";
+      img.style.display = "none";
+    }
+  })
+  .catch(err => console.error(err));
