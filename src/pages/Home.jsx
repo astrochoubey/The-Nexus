fd8d04ba8147dded2041ec8f09ae0ed3f20a0fb4
Prachi Choubey <prachichoubey.thenexusphysics@gmail.com>
2026-07-24 07:38:46 +0530

    tailwindcss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 export const Home = () => {
     return (
-        <>
-            
-        </>
+        <div className = "min-h-screen ">
+            {/* {header} */}
+            {/* {background effects} */}
+            {/* {navbar} */}
+            {/* {hero sec} */}
+            {/* {footer} */}
+        </div>
     )
 }
